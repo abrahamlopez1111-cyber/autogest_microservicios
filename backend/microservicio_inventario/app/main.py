0d71e5323893fc0cfd6315a1f2c342033510302f
dayana160 <dayanasandon9@gmail.com>
2026-04-15 10:30:22 -0500

    cambios

--- backend/microservicio_inventario/app/main.py
+++ backend/microservicio_inventario/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI # type: ignore
-from .routes import inventario
+from app.database import engine, Base
+from app.routes import inventario
 
-app = FastAPI(title="Microservicio de Repuestos")
+app = FastAPI(
+    title="Microservicio Inventario",
+    version="1.0"
+)
 
+# Crear tablas
+Base.metadata.create_all(bind=engine)
+
+# Rutas
 app.include_router(inventario.router)
+
+@app.get("/")
+def root():
+    return {"mensaje": "Microservicio Inventario funcionando"}
--- backend/microservicio_inventario/app/models.py
+++ backend/microservicio_inventario/app/models.py
@@ -1,31 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey # type: ignore
-from .database import Base
+from sqlalchemy import Column, Integer, String, Float # type: ignore
+from app.database import Base
 
-class CatalogoRepuesto(Base):
+
+class CatalogoRepuestos(Base):
+
     __tablename__ = "catalogo_repuestos"
 
     id = Column(Integer, primary_key=True, index=True)
-    codigo_inventario = Column(String(50), unique=True)
-    descripcion = Column(String(255))
-    requiere_lote = Column(Boolean, default=False)
-    punto_pedido_global = Column(Integer, default=0)
-
-
-class StockSucursal(Base):
-    __tablename__ = "stock_sucursal"
-
-    id = Column(Integer, primary_key=True, index=True)
-    sucursal_id = Column(Integer)
-    catalogo_repuestos_id = Column(Integer, ForeignKey("catalogo_repuestos.id"))
-    cantidad_disponible = Column(Integer)
-
-
-class Transferencia(Base):
-    __tablename__ = "transferencias"
-
-    id_transferencias = Column(Integer, primary_key=True, index=True)
-    catalogo_repuestos_id = Column(Integer, ForeignKey("catalogo_repuestos.id"))
-    sucursal_origen_id = Column(Integer)
-    sucursal_destino_id = Column(Integer)
-    cantidad = Column(Integer)
-    estado = Column(String(50))
+    codigo_inventario = Column(String, nullable=False)
+    descripcion = Column(String, nullable=False)
+    precio = Column(Float, nullable=False)
+    stock = Column(Integer, nullable=False)
